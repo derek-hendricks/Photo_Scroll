3ab47e90516ca9350f7bf6493c69a0a6439a7bbb
derek-hendricks <derek.m.hendricks@gmail.com>
2014-11-10 02:41:04 -0700

    add priority column to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,7 @@ class Task < ActiveRecord::Base
   validates :name, :description, :duration, :presence => true
   validates :name, :uniqueness => true
   validates_numericality_of :duration, {:greater_than_or_equal_to => 1, :less_than_or_equal_to => 240}
+  validates_numericality_of :priority, {:greater_than_or_equal_to => 1, :less_than_or_equal_to => 10}
   validate :due_in_future, :on=>:create
 
   scope :search_by_name_and_description, -> search { where("name like ? OR description like ?", "%#{search}%", "%#{search}%") }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110044900) do
+ActiveRecord::Schema.define(version: 20141110093656) do
 
   create_table "images", force: true do |t|
     t.string   "caption"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141110044900) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "priority"
   end
 
   create_table "users", force: true do |t|
