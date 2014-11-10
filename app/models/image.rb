3bbe25d0825245051f611706923c0f7ba98f1ae4
derek-hendricks <derek.m.hendricks@gmail.com>
2014-11-09 19:14:37 -0700

    add validations to Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,5 @@
 class Image < ActiveRecord::Base
+  validates(:caption, :description, :url, :rating, :presence => true)
+  validates(:caption, :url, :uniqueness => true)
+  validates_numericality_of :rating, {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 100}
 end
