1170c0692c8860bfaba7b99979be9b2b2307900f
derek-hendricks <derek.m.hendricks@gmail.com>
2014-11-09 19:11:43 -0700

    add validations to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ActiveRecord::Base
+  validates :name, :description, :duration, :presence => true
+  validates :name, :uniqueness => true
+  validates_numericality_of :duration, {:greater_than_or_equal_to => 1, :less_than_or_equal_to => 240}
 end
