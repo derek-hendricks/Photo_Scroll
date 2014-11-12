fc220eea016a568c5f6c0f0c0d5701124d184800
derek-hendricks <derek.m.hendricks@gmail.com>
2014-11-11 17:52:24 -0700

    create test for follow feature in author model

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user=authors(:basic_user)
+		@derek=authors(:derek)
+	end
+
+	test "user can follow derek" do
+		@user.follows << @derek
+		assert @user.follows.include?(@derek)
+	end
+	
+	test "user can unfollow derek" do
+		@user.follows << @derek
+		@user.follows.delete(@derek)
+		assert ! @user.follows.include?(@derek)
+	end
+
 end
