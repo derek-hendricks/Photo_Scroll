b22dfb7824812d0e88661979cf5bbb4778576e87
derek-hendricks <derek.m.hendricks@gmail.com>
2014-11-14 19:30:51 -0700

    add author full_name validation test

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -3,11 +3,16 @@ require 'test_helper'
 class AuthorTest < ActiveSupport::TestCase
 
 	def setup
-		@user=authors(:basic_user)
-		@derek=authors(:derek)
+		@user = authors(:basic_user)
+		@derek = authors(:derek)
 	end
-	
-  # @user.follows reads records in author_follows table that have author_id set to @user.id and then
+
+	test "full name should be present" do
+    @user.full_name = "     "
+    assert_not @user.valid?
+  end
+
+	# @user.follows reads records in author_follows table that have author_id set to @user.id and then
   # finds associated authors records with id matching follow_id
 	test "user can follow derek" do
 		@user.follows << @derek
