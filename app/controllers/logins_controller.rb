0f03f6893b650e5ebfd1c5a0a7d85ab84cca893e
Derek Hendricks <derek.m.hendricks@gmail.com>
2014-11-15 23:04:32 +0000

    add development tests

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,20 +1,20 @@
 class LoginsController < ApplicationController
 
   def login
-	username = params[:username]
-	password = params[:password]
-	author = Author.where("lower(username) = ?", username.downcase).first
-	if author != nil && (password.casecmp(author.password) == 0)
-		session[:author_id] = author.id
-		if author.admin 
-			redirect_to authors_url 
+		username = params[:username]
+		password = params[:password]
+		author = Author.where("lower(username) = ?", username.downcase).first
+		if author && author.authenticate(params[:password])
+			session[:author_id] = author.id
+			if author.admin 
+				redirect_to authors_url 
+			else
+				redirect_to messages_url
+			end
 		else
-			redirect_to messages_url
+			redirect_to login_url, :alert => "Unknown username/password"
 		end
-	else
-		redirect_to login_url, :alert => "Unknown username/password"
 	end
-  end
 
   def logout
   	session[:author_id] = nil
