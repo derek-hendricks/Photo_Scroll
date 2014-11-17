8d979990b77ca21697180bd53888c559f65bac94
Derek Hendricks <derek.m.hendricks@gmail.com>
2014-11-17 01:32:13 +0000

    add gravatar images for authors

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,7 @@
 module MessagesHelper
+  def gravatar_for(author)
+    gravatar_id = Digest::MD5::hexdigest(author.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: author.full_name, class: "gravatar")
+  end
 end
