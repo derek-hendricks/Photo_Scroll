e5e8a5d4a7eaf467ce1de25ea7854602520e71ab
derek-hendricks <derek.m.hendricks@gmail.com>
2014-11-18 18:10:15 -0700

    cllean up

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -9,18 +9,18 @@ class LoginsController < ApplicationController
 	def create 
 		author = Author.find_by(username: params[:login][:username].downcase)
 		if author && author.authenticate(params[:login][:password])
-    	log_in author
-    	params[:login][:remember_me] == '1' ? remember(author) : forget(author)
-    	send_back_or author
-    else
-    	flash.now[:danger] = "wrong credentials"
+	    log_in author
+	    params[:login][:remember_me] == '1' ? remember(author) : forget(author)
+	    send_back_or author
+	  else
+	    flash.now[:danger] = "wrong credentials"
 			render 'new'
 		end
 	end
-	
+		
 	def destroy 
 		log_out if logged_in?
-    redirect_to root_url
+	   redirect_to root_url
 	end
 
 end
