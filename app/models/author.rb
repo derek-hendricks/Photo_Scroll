2b414729b3a92ba3cfa7624e7a1bd4413b12417e
derek-hendricks <derek.m.hendricks@gmail.com>
2014-11-18 18:12:34 -0700

    Update author.rb

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,14 +3,14 @@ class Author < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
   validates :username, uniqueness: true, presence: true, length: { maximum: 30 }
-	validates :full_name,  presence: true, length: { maximum: 50 }
+  validates :full_name,  presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6, maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
    
-	has_and_belongs_to_many :favourites, :join_table => "favourites", :class_name => "Message", :foreign_key => "fav_author_id"
+  has_and_belongs_to_many :favourites, :join_table => "favourites", :class_name => "Message", :foreign_key => "fav_author_id"
 	has_many :author_follows
 	has_many :follows, :through => :author_follows
 	has_many :follow_authors, :class_name => "AuthorFollow", :foreign_key => :follow_id 
