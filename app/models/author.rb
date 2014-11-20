7da1c4c692293efc933d74d8af0dbbf665780d39
derek-hendricks <derek.m.hendricks@gmail.com>
2014-11-20 00:44:10 -0700

    resolve conflicts

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,7 +3,6 @@ class Author < ActiveRecord::Base
   before_save   :downcase_email
   before_create :create_activation_digest
 
-
   validates :username, uniqueness: true, presence: true, length: { maximum: 30 }
   validates :full_name,  presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6, maximum: 30 }
@@ -22,53 +21,36 @@ class Author < ActiveRecord::Base
   def followed_messages 
     Message.followed_by(self.id)
   end
-	
-<<<<<<< HEAD
-	def Author.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  	end
-  	
- 	def Author.new_token
-    SecureRandom.urlsafe_base64
-  	end
-=======
+  
   def Author.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+    end
+    
+  def Author.new_token
+    SecureRandom.urlsafe_base64
+    end
+    
+  def remember
+    self.remember_token = Author.new_token
+    update_attribute(:remember_digest, Author.digest(remember_token))
   end
-  	
- def Author.new_token
-   SecureRandom.urlsafe_base64
-  end
->>>>>>> 8468577dc4c690258a896e1882d4f842f872a336
-  	
- def remember
-   self.remember_token = Author.new_token
-   update_attribute(:remember_digest, Author.digest(remember_token))
- end
   
-<<<<<<< HEAD
  # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
-=======
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
->>>>>>> 8468577dc4c690258a896e1882d4f842f872a336
   end
   
   def forget
     update_attribute(:remember_digest, nil)
   end
   
-	def activate
+  def activate
     update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
-	end
+  end
 
   def send_activation_email
     AuthorMailer.account_activation(self).deliver
@@ -90,15 +72,15 @@ class Author < ActiveRecord::Base
   
   private 
   
-  	def downcase_email
-  		self.email = email.downcase
-  	end
+    def downcase_email
+      self.email = email.downcase
+    end
 
     # Creates and assigns the activation token and digest.
-		def create_activation_digest
-			self.activation_token  = Author.new_token
-			self.activation_digest = Author.digest(activation_token)
-		end
+    def create_activation_digest
+      self.activation_token  = Author.new_token
+      self.activation_digest = Author.digest(activation_token)
+    end
   
 end
 
