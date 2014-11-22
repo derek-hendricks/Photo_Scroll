8e7dffd8a59f281d6ddaa5b71d677cac97168ee7
Derek Hendricks <derek.m.hendricks@gmail.com>
2014-11-22 08:35:10 +0000

    add endpoint to carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,9 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region                 => 'us-west-2'
+      :region                 => 'us-east-1',
+      :endpoint               => 'photoderek.s3-website-us-east-1.amazonaws.com'
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+ 
   end
 end
